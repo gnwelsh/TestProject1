600d4b81ce4742c611e2259ac53ab661497725a4
phdwelsh <pwelsh1130@gmail.com>
2020-07-06 13:54:15 -0400

    small change - test of commit and push

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -4,7 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Git Hub Test!!" + "push test");
-		System.out.println("Hello World!!!");
+		System.out.println("Hello World!");
 
 	}
 
